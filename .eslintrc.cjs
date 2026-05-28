0c43b11cf10ca080e524691f971688137efea740
HealthPulse Dev <dev@healthpulse.local>
2026-05-28 22:41:14 +0800

    chore: add eslint and prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2022, sourceType: "module" },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", "build", "node_modules", "coverage"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
